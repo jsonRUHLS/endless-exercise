2f4d58d05cb976b9c54954387b37f1e89c6aa397
Jason <jsonruhls@gmail.com>
2021-05-17 17:21:59 -0400

    add how-it-works data and style

--- src/app/how-it-works/how-it-works.component.ts
+++ src/app/how-it-works/how-it-works.component.ts
@@ -9,11 +9,41 @@ import { StepsService } from '../core/services/steps.service';
 })
 export class HowItWorksComponent implements OnInit {
   public steps: ISteps[];
-  constructor(private stepsService: StepsService) {}
+
+  // eslint-disable-next-line prettier/prettier
+  constructor(private stepsService: StepsService) { }
 
   ngOnInit(): void {
-    this.stepsService
-      .getSteps()
-      .subscribe((data: ISteps[]) => (this.steps = data));
+    this.stepsService.getSteps().subscribe((data: ISteps[]) => {
+      this.steps = data.sort((a, b) =>
+        a.stepNumber.localeCompare(b.stepNumber)
+      );
+
+      // here I sort the data by effectiveDate
+      this.steps.forEach(() => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const sortByDate = (data: any) => {
+          const sorter = (
+            a: { effectiveDate: string | number | Date },
+            b: { effectiveDate: string | number | Date }
+          ) => {
+            return (
+              new Date(a.effectiveDate).getTime() -
+              new Date(b.effectiveDate).getTime()
+            );
+          };
+          data.sort(sorter);
+          return data;
+        };
+        sortByDate(data);
+
+        // I haven't been able to figure out how to get
+        // to the versionContent to be able to slice off the last
+        // element to use for the data
+        // if (this.steps.versionContent.length > 1) {
+        //   this.steps.versionContent.length - 1;
+        // }
+      });
+    });
   }
 }
